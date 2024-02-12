8b47737b01ac464bd76d019af437ae44b8fd96fa
Ravindu Karunaweera <94894276+RavinduLK@users.noreply.github.com>
2024-02-12 14:28:55 +0530

    Initial commit

---
+++ src/main/java/NMSTechVenturas/example/NMSTechVenturas/NmsTechVenturasApplication.java
@@ -0,0 +1,13 @@
+package NMSTechVenturas.example.NMSTechVenturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmsTechVenturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmsTechVenturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NMSTechVenturas/example/NMSTechVenturas/NmsTechVenturasApplicationTests.java
@@ -0,0 +1,13 @@
+package NMSTechVenturas.example.NMSTechVenturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmsTechVenturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
